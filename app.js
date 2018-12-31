a88bf588a8fdfa8a9f35f8b0035cfbb0c44556a7
Marsel Gray <marselgray@gmail.com>
2018-12-30 19:22:16 -0500

    Reading Input Data

--- app.js
+++ app.js
@@ -9,7 +9,30 @@ var budgetController = (function(){
 
 // UI Controller
 var UIController = (function(){
-    // some code will go here later
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    
+    return {
+        getinput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
+
+
 })();
 
 
@@ -17,24 +40,30 @@ var UIController = (function(){
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
+    //pass DOMstrings down into this function
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function (){
         // 1. get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
         // 2. add the item to the budget controller
         // 3. add the item to the UI 
         // 4. calculate the budget
         // 5. display the budget  on the UI
-        console.log('it works');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 
     document.addEventListener('keypress', function(event){
         
-        //older browsers don't use 'keycode' but use 'which'
-        if (event.keyCode === 13 || event.which === 13 ){
+        //older browsers don't use 'keypress' but use 'which' https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/which
+        // keycode is also deprecated https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+        if (event.keypress === 13 || event.which === 13 ){
             ctrlAddItem();
         } 
+
     });
 
 })(budgetController, UIController);
